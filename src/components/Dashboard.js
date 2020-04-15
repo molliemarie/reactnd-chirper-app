7d80958d959960eafe52ef2dbd81e8f4f6f08309
Mollie Marie Pettit <mollie.marie@gmail.com>
2020-04-15 16:24:14 -0700

    displaying tweet ids in dashboard

---
+++ src/components/Dashboard.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+class Dashboard extends Component {
+    render() {
+        return (
+            <div>
+                <h3 className='center'>Your Timeline</h3>
+                <ul className='dashboard-list'>
+                    {this.props.tweetIds.map((id) => (
+                        <li key={id}>
+                            <div>TWEET ID: {id}</div>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        )
+    }
+}
+
+// take in state of store (data needed here is tweets)
+// returns an object that has a tweets id property on it
+// sort tweets by timestamp
+function mapStateToProps({ tweets }) {
+    return {
+        tweetIds: Object.keys(tweets)
+            .sort((a, b) => tweets[b].timestamp - tweets[a].timestamp)
+    }
+}
+
+export default connect(mapStateToProps)(Dashboard) 
